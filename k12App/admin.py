88e3fffbc7608562715091574de4c06aa36b453e
Sushma <sushyad95@gmail.com>
2020-08-25 15:34:52 +0530

    brach changes

--- k12App/admin.py
+++ k12App/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User, Question, Answer, Upvote
 
+# newest change
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Question)
